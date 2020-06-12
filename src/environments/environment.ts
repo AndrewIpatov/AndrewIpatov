f3b2e1c09f79644070ddb92e2b740a618d0ff654
AndrewIpatov <ipatov123321@gmail.com>
2020-06-12 10:58:27 +0300

    generated deployables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+firebaseConfig:{
+      apiKey: "AIzaSyBxDmvhnylByHeCfDGXYnS4eQ7P-0ILgyI",
+      authDomain: "ng-test-app-e2b8d.firebaseapp.com",
+      databaseURL: "https://ng-test-app-e2b8d.firebaseio.com",
+      projectId: "ng-test-app-e2b8d",
+      storageBucket: "ng-test-app-e2b8d.appspot.com",
+      messagingSenderId: "603444947053",
+      appId: "1:603444947053:web:db4f81c92c54d75077b764"
+    }
 };
 
 /*
